8f9c3a42f984d1d6c7059ebe12244e58f2a73798
Heric_ <herico_campos@hotmail.com>
2023-04-25 23:19:47 -0300

    Adicionado userHandler

---
+++ src/Core/UserHandler.js
@@ -0,0 +1,17 @@
+const { User, userSchema } = require("../model/UserModel");
+
+class UserHandler {
+  constructor() {}
+
+  async registerUser(req) {}
+
+  async updateUser(req) {}
+
+  async deleteUser(req) {}
+
+  async fetchUsers() {}
+
+  async fetchSpecificUsers(req) {}
+}
+
+module.exports = UserHandler;
--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
-    console.log(req.body);
+    
   } catch (error) {
     console.log(JSON.stringify(error));
     return res.status(404).json(JSON.stringify(error));
