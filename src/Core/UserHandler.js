38a33eb0d66f26b5052425deb0b08c21ff261a1a
Heric_ <herico_campos@hotmail.com>
2023-04-26 00:01:11 -0300

    Add req create,getAll

--- src/Core/UserHandler.js
+++ src/Core/UserHandler.js
@@ -1,15 +1,37 @@
-const { User, userSchema } = require("../model/UserModel");
+const { User: UserModel } = require("../model/UserModel");
 
 class UserHandler {
   constructor() {}
 
-  async registerUser(req) {}
+  async registerUser(req) {
+    try {
+      const userbody = {
+        user: req.body.user,
+        email: req.body.email,
+        password: req.body.password,
+      };
+
+      await UserModel.create(userbody);
+
+      return { msg: "usuario criado com sucesso" };
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async updateUser(req) {}
 
   async deleteUser(req) {}
 
-  async fetchUsers() {}
+  async fetchUsers() {
+    try {
+      const users = await UserModel.find();
+
+      return users;
+    } catch (error) {
+      return console.log(error);
+    }
+  }
 
   async fetchSpecificUsers(req) {}
 }
--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const UserHandler = require("../Core/UserHandler");
+const handler = new UserHandler();
 
-router.post("/register", async (req, res) => {
+router.post("/userRegister", async (req, res) => {
   try {
-    
+    const user = await handler.registerUser(req);
+    if (user) return res.status(201).send(user);
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    return res.status(404).json(JSON.stringify(error));
+  }
+});
+
+router.get("/getAll", async (req, res) => {
+  try {
+    const user = await handler.fetchUsers();
+    if (user) return res.status(200).send(user);
   } catch (error) {
     console.log(JSON.stringify(error));
     return res.status(404).json(JSON.stringify(error));
